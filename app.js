d5cd1ef59870acc938a7a723ebc709345a06d94f
thakurnitin2684 <thakurnitin2684@gmail.com>
2022-05-22 13:22:42 +0530

    loggin

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
+  console.log("here 1");
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
@@ -29,9 +30,10 @@ app.post("/", function (req, res) {
       },
     ],
   };
-
+  console.log("here 2");
   const jsonData = JSON.stringify(data);
 
+  console.log("here 3");
   var url = "https://us14.api.mailchimp.com/3.0/lists/f6d85d2ccb";
   const options = {
     method: "POST",
@@ -43,14 +45,15 @@ app.post("/", function (req, res) {
       res.sendFile(__dirname + "/success.html");
     } else {
       console.log(response);
+      console.log("here 4");
       res.sendFile(__dirname + "/failure.html");
     }
 
     response.on("data", function (data) {
-      console.log(JSON.parse(data));
+      // console.log(JSON.parse(data));
     });
   });
-
+  console.log("here 5");
   request.write(jsonData);
   request.end();
 });
